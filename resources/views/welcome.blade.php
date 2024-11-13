8fa649fe840540a670fec56fff2a01e7b59d6e39
AbdullahHouso <abdullahhouso@gmail.com>
2024-11-13 10:25:58 +0300

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="flex flex-col">
-        <h1 class="text-xl font-bold text-emerald-600">LockNext Admin Panel</h1>
+        <h1 class="text-xl font-bold text-emerald-600"></h1>
         <p class="text-lg font-base">
             This is a demonstration prototype for LockNext admin panel. It is currently for demonstration purposes only, We are looking to expand it in many ways with many promising features.
             <br>
